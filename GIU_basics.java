2afa857ecce9e8d9cf336c07bbdfa1892e70c81f
WedithaV <wedithavishwawiz@gmail.com>
2024-10-15 19:58:22 +0530

    GIU-Basics

---
+++ GIU_basics.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class GIU_basics {
+  public static void main(String[] args){
+    String name = JOptionPane.showInputDialog("Enter your name");
+    JOptionPane.showMessageDialog(null,"Hello "+name);
+    
+    int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+    // convert string to a int
+    JOptionPane.showMessageDialog(null,"Your age is "+age);
+
+    float height = Float.parseFloat(JOptionPane.showInputDialog("Enter your height"));
+    JOptionPane.showMessageDialog(null,"Your height is "+height);
+
+  }  
+}
