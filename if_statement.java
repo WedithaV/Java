a8eef18e9a67386112dc9e41cecd53afd1fc2057
WedithaV <wedithavishwawiz@gmail.com>
2024-10-15 22:43:55 +0530

    if statement

---
+++ if_statement.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class if_statement {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your age:");
+    int x = scanner.nextInt();
+
+    if( x < 0){
+      System.out.println("Age can not be negative");
+    }
+    else if(x < 18){
+      System.out.println("You are a child");
+    }
+    else if(x >= 18 && x < 75){
+      System.out.println("You are adult now");
+    }
+    else if(x >= 75 && x < 120){
+      System.out.println("You are a great elder now");
+    }
+    else{
+      System.out.println("You can not be alive");
+    }
+  }
+}
