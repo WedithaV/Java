89f688e242fae49cdf4775a0091b9697a606f554
WedithaV <wedithavishwawiz@gmail.com>
2024-10-15 23:34:45 +0530

    switch statement

---
+++ switch_statement.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class switch_statement {
+  public static void main(){
+    String date = JOptionPane.showInputDialog("Enter Day");
+    switch(date){
+      case "Sunday":
+      JOptionPane.showMessageDialog(null,"Sun");
+      break;
+      case "Monday":
+      JOptionPane.showMessageDialog(null,"Mon");
+      break;
+      case "Tuesday":
+      JOptionPane.showMessageDialog(null,"Tue");
+      break;
+      default:
+      JOptionPane.showMessageDialog(null,"A day");
+    }
+  }
+}
