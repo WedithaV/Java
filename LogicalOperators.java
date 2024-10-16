87010b0a3dd61932b9d85680d1302abdd864b0f5
WedithaV <wedithavishwawiz@gmail.com>
2024-10-16 20:56:41 +0530

    Logical Operators

---
+++ LogicalOperators.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class LogicalOperators{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter number:");
+    int x = scanner.nextInt();
+    scanner.nextLine();
+
+    if(x > 0 && x < 10){
+      System.out.println("X is between 0 and 10");
+    }
+    else{
+      System.out.println("X is not between 0 and 10");
+    }
+
+    System.out.println("Do you want to quit ? press 'q' or 'Q'");
+    String response = scanner.next(); //next(); only read one word
+
+    if(response.equals("Q") || response.equals("q")){
+      System.out.println("Quit the game");
+    }
+    else{
+      System.out.println("Still playing");
+    }
+
+
+    
+  }
+}
