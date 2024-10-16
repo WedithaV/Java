3daeef1a42188cecf4f96311268233b9e04b2165
WedithaV <wedithavishwawiz@gmail.com>
2024-10-16 21:40:12 +0530

    WhileLoop

---
+++ WhileLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+  public static void main(){
+    Scanner scanner = new Scanner(System.in);
+    String name = "";
+
+    while(name.isBlank()){
+      System.out.println("Enter your name :");
+      name = scanner.nextLine();
+    }
+    System.out.println("Hello "+name);
+
+    String dtx = "";
+
+    do{
+      System.out.println("Enter a character:");
+      dtx = scanner.nextLine();
+    }while(dtx.isBlank());
+  }
+}
