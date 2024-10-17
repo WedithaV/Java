4b38c273565721a1199631b140d4bc55288e6ef5
WedithaV <wedithavishwawiz@gmail.com>
2024-10-17 12:00:32 +0530

    2D Arrays

--- Arrays.java
+++ Arrays.java
@@ -1,12 +1,14 @@
 public class Arrays {
   public static void main(String[] args){
+    // 1D ARRAYS
+
     // Method 1
     String[] cars = {"Tesla", "Benz", "BMW", "Audi"};
     for(int i = 0; i<cars.length; i++){
       System.out.println(cars[i]);
     }
     System.out.println();
-    
+
     // Method 2
     String[] names = new String[3];
     names[0] = "Weditha";
@@ -14,6 +16,44 @@ public class Arrays {
     names[2] = "Prabhath";
     for(int i=0; i<names.length; i++){
       System.out.println(names[i]);
+    }
+
+    // 2D Arrays
+
+    // Method 1
+    String[][] letters = new String[3][3];
+
+    letters[0][0] = "A";
+    letters[0][1] = "B";
+    letters[0][2] = "C";
+    letters[1][0] = "D";
+    letters[1][1] = "E";
+    letters[1][2] = "F";
+    letters[2][0] = "G";
+    letters[2][1] = "H";
+    letters[2][2] = "I";
+
+    for(int i = 0; i < letters.length; i++){
+      System.out.println();
+      for(int j = 0; j < letters[i].length; j++){
+        System.out.print(letters[i][j]+"\t");
+      }
+    }
+
+    //Method 2
+
+    System.out.println();
+    int[][] numbers = {
+                            {1,2,3},
+                            {4,5,6},
+                            {7,8,9}
+                        };
+
+    for(int i = 0; i < numbers.length; i++){
+      System.out.println();
+      for(int j = 0; j < numbers[i].length; j++){
+        System.out.print(numbers[i][j]+"\t");
+      }
     }
   }
 }
