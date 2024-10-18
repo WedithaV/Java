ae63fc0f07e04d1ef704dc6dcd2822aaddc280df
WedithaV <wedithavishwawiz@gmail.com>
2024-10-18 16:58:43 +0530

    2D ArrayList

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.*; // For 2D arraylists
 
 public class ArrayLists {
   public static void main(String[] args){
@@ -20,17 +21,44 @@ public class ArrayLists {
       System.out.print(numbers.get(i)+"\t");
     }
     System.out.println();
+    System.out.println(numbers);
 
     numbers.set(0,0); // change elements
-    for(int i = 0; i < numbers.size(); i++){
-      System.out.print(numbers.get(i)+"\t");
-    }
-    System.out.println();
+    System.out.println(numbers);
 
     numbers.remove(2); // remove items
-    for(int i = 0; i < numbers.size(); i++){
-      System.out.print(numbers.get(i)+"\t");
-    }
+    System.out.println(numbers);
     System.out.println();
+
+    /*-------------------------------------------------------------------------------*/
+
+    // 2D ArrayList
+
+    ArrayList<ArrayList<String>> groceryList = new ArrayList<ArrayList<String>>();
+
+    ArrayList<String> vegetableList = new ArrayList<String>();
+    vegetableList.add("Pumkin");
+    vegetableList.add("Beans");
+    vegetableList.add("carrot");
+
+    ArrayList<String> sweetList = new ArrayList<String>();
+    sweetList.add("cake");
+    sweetList.add("watalappan");
+    sweetList.add("Keju");
+
+    ArrayList<String> drinkList = new ArrayList<String>();
+    drinkList.add("Cocacola");
+    drinkList.add("Pepsi");
+
+    groceryList.add(vegetableList);
+    groceryList.add(sweetList);
+    groceryList.add(drinkList);
+
+    System.out.println(groceryList); // Access full  2D arraylist
+    System.out.println(groceryList.get(1)); //Acess sub ArrayList
+    System.out.println(groceryList.get(0).get(0)); // Access an Element
+
+    // Do not want to add the same number of elements as the other arraylists
+    
   }
 }
