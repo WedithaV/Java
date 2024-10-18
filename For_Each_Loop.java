998a29650aa231e71de8db646874a0aec3a00a82
WedithaV <wedithavishwawiz@gmail.com>
2024-10-18 21:11:36 +0530

    For Each Loop

---
+++ For_Each_Loop.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class For_Each_Loop {
+  public static void main(String[] args){
+    String[] names = {"Weditha","Vishwa","Prabhath"};
+
+    for(String i : names){
+      System.out.println(i);
+    }
+    System.out.println();
+
+    ArrayList<String> drinkList = new ArrayList<String>();
+    drinkList.add("Cocacola");
+    drinkList.add("Pepsi");
+    drinkList.add("Stin");
+
+    for(String i : drinkList){
+      System.out.println(i);
+    }
+    System.out.println();
+  }
+}
