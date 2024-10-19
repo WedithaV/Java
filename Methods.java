75e80f49a853b8d484f489df6d53daedd4289ccc
WedithaV <wedithavishwawiz@gmail.com>
2024-10-19 10:03:15 +0530

    Methods

---
+++ Methods.java
@@ -0,0 +1,15 @@
+public class Methods {
+  public static void main(String[] args){
+
+    hello("Weditha");
+    System.out.println(add(9,1));
+  }
+
+  static void hello(String name){
+    System.out.println("Hello "+name);
+  }
+
+  static int add(int a,int b){
+    return a+b;
+  }
+}
