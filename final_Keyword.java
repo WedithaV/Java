e97c27f95de751aedc691a03fb7a0045796dcbf0
WedithaV <wedithavishwawiz@gmail.com>
2024-10-19 15:52:12 +0530

    Final keyword

---
+++ final_Keyword.java
@@ -0,0 +1,8 @@
+public class final_Keyword {
+  public static void main(String[] args){
+    final double Pi = 3.14; //Static, can not change the value 
+    System.out.println(Pi);
+
+    pi = 4; // When try to change the value, it gives an error
+  }
+}
