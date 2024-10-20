8147c1eb1a9d3e9791a52f77f81c2f875a3600ff
WedithaV <wedithavishwawiz@gmail.com>
2024-10-20 23:20:16 +0530

    variable scope

---
+++ VariableScope.java
@@ -0,0 +1,14 @@
+public class VariableScope {
+  public static void main(String[] args){
+
+  }
+}
+
+class dog{
+  String name = "Tom"; //Global variable
+  void bark(){
+    int time = 10; //local variable
+  }
+}
+
+
