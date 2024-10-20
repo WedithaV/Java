ec12da901f78f8e1cb259b34b42f102e15d8276f
WedithaV <wedithavishwawiz@gmail.com>
2024-10-20 11:39:50 +0530

    Constructors

---
+++ constructors.java
@@ -0,0 +1,33 @@
+public class constructors {
+  public static void main(String[] args){
+    //constructor =  special method that is called when an object is instantiated (created)
+    //It helps to create objects with different attributes
+    Human human1 = new Human("Weditha",24,76);
+    Human human2 = new Human("Kivindu",18,80);
+
+    System.out.println(human1.name);
+    System.out.println(human2.name);
+    System.out.println();
+    human1.drink();
+    human2.eat();
+  }
+}
+
+class Human{
+  String name;
+  int age;
+  double weight;
+
+  Human(String name, int age, double weight){
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+  }
+
+  void eat(){
+    System.out.println(this.name+" is eating");
+  }
+  void drink(){
+    System.out.println(this.name+" is drinking");
+  }
+}
