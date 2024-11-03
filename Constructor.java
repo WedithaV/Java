ae9d4b7a240cdf280980b7292dd75b19aca5ea6b
WedithaV <wedithavishwawiz@gmail.com>
2024-11-03 22:19:55 +0530

    def constructors

--- Constructor.java
+++ Constructor.java
@@ -1,6 +1,6 @@
 public class Constructor {
   public static void main(String[] args){
-    //constructor =  special method that is called when an object is instantiated (created)
+    //constructor =  constructor is a specila method type which has no return type
     //It helps to create objects with different attributes
     Human human1 = new Human("Weditha",24,76);
     Human human2 = new Human("Kivindu",18,80);
