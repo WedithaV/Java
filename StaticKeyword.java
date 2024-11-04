e36b57f48d5c71c1c476ba05d12f8957ca9a2f66
WedithaV <wedithavishwawiz@gmail.com>
2024-11-04 11:24:37 +0530

    Static keyword

---
+++ StaticKeyword.java
@@ -0,0 +1,39 @@
+/*
+ * In Java, the static keyword makes a member (variable, method, block, or class)
+ *  belong to the class itself rather than any specific instance of the class:
+
+   Static Variables: Shared across all objects of the class. Only one copy exists.
+   Example: static int count;.
+
+   Static Methods: Can be called without creating an object of the class. 
+   They can only directly access other static members(cannot access instance variables).
+  Example: ClassName.methodName();.
+*/
+public class StaticKeyword{
+  public static void main(String[] args){
+    Friends.numOfFriends = 1; // can access without initiating a object
+    Friends.display();
+
+    Friends friend1 = new Friends();
+    Friends friend2 = new Friends();
+    friend1.addFriend("Weditha");
+    Friends.display();
+    friend2.addFriend("Vishwa");
+    Friends.display();
+
+  }
+}
+
+class Friends{
+  String nameOfFriend;
+  static int numOfFriends;
+
+  void addFriend(String nameOfFriend){
+    this.nameOfFriend = nameOfFriend;
+    numOfFriends++;
+  }
+  static void display(){
+    System.out.println(numOfFriends);
+//  System.out.println(nameOfFriend);  cannot access instances variables
+  }
+}
