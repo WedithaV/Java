d75bd91386066a9d71a4127b5e175d316c0617ff
WedithaV <wedithav@gmail.com>
2025-02-02 12:52:08 +0530

    StringBuffer

---
+++ StringBufferrr.java
@@ -0,0 +1,8 @@
+public class StringBufferrr {
+    public static void main(String[] args){
+        StringBuffer sb1 = new StringBuffer("Weditha");
+        System.out.println(sb1);
+        System.out.println(sb1.capacity());
+        System.out.println(sb1.length());
+    }
+}
